16a97d8b8fea558b9ab2fcd43792c52566a3d7af
lucianocolin <lucianocolin99@gmail.com>
2025-05-20 22:01:30 -0300

    Add user mapper for responses

---
+++ src/modules/auth/application/mapper/user.mapper.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../../domain/user.domain';
+import { UserResponseDto } from '../dto/user-response.dto';
+
+@Injectable()
+export class UserMapper {
+  fromUserToUserResponseDto(user: User): UserResponseDto {
+    const { id, fullName, email, phoneNumber, roles } = user;
+
+    return new UserResponseDto({
+      id,
+      fullName,
+      email,
+      phoneNumber,
+      roles,
+    });
+  }
+}
