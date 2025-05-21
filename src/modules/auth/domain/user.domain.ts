15da1124d39e7da538cef80b19d53a6335312166
lucianocolin <lucianocolin99@gmail.com>
2025-05-20 22:02:11 -0300

    Add user domain

---
+++ src/modules/auth/domain/user.domain.ts
@@ -0,0 +1,10 @@
+import { USER_ROLES } from '../application/enum/user-roles.enum';
+
+export class User {
+  id: string;
+  fullName: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  roles: USER_ROLES[];
+}
