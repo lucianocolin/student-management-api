38e192a911ad5b31fe4dffc26a1cca1b474adb98
lucianocolin <lucianocolin99@gmail.com>
2025-05-23 21:05:23 -0300

    Add student module tests

--- test/test-app.module.ts
+++ test/test-app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../src/modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserEntity } from '../src/modules/auth/infrastructure/database/user.entity';
+import { StudentEntity } from '../src/modules/student/infrastructure/database/student.entity';
+import { StudentModule } from '../src/modules/student/student.module';
 
 @Module({
   imports: [
@@ -20,8 +23,10 @@ import { ConfigModule } from '@nestjs/config';
       synchronize: true,
       autoLoadEntities: true,
       logging: false,
+      entities: [UserEntity, StudentEntity],
     }),
     AuthModule,
+    StudentModule,
   ],
 })
 export class TestAppModule {}
