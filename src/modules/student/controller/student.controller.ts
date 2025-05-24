53585f6e9cc6bf938c603f424e9af9c0df09a2e9
lucianocolin <lucianocolin99@gmail.com>
2025-05-23 21:03:46 -0300

    Add student controller

---
+++ src/modules/student/controller/student.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import {
+  IStudentService,
+  STUDENT_SERVICE_KEY,
+} from '../domain/interfaces/student-service.interface';
+import { StudentResponseDto } from '../application/dto/student-response.dto';
+import { STUDENT_NAME } from '../domain/student.name';
+import { Auth } from '../../auth/decorators/auth.decorator';
+import { USER_ROLES } from '../../auth/application/enum/user-roles.enum';
+import { CreateStudentDto } from '../application/dto/create-student.dto';
+
+@Controller(STUDENT_NAME)
+export class StudentController {
+  constructor(
+    @Inject(STUDENT_SERVICE_KEY)
+    private readonly studentService: IStudentService,
+  ) {}
+
+  @Auth(USER_ROLES.ADMIN)
+  @Get()
+  async getAll(): Promise<StudentResponseDto[]> {
+    return await this.studentService.getAll();
+  }
+
+  @Auth()
+  @Get(':id')
+  async getOneById(id: string): Promise<StudentResponseDto> {
+    return await this.studentService.getOneById(id);
+  }
+
+  @Auth()
+  @Post()
+  async create(
+    @Body() createStudentDto: CreateStudentDto,
+  ): Promise<StudentResponseDto> {
+    return await this.studentService.create(createStudentDto);
+  }
+}
