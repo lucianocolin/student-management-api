618016967514e93e03865da0568d970c0798d7f7
lucianocolin <lucianocolin99@gmail.com>
2025-05-25 21:55:33 -0300

    Add career module in student one

--- src/modules/student/student.module.ts
+++ src/modules/student/student.module.ts
@@ -8,6 +8,7 @@ import { StudentEntity } from './infrastructure/database/student.entity';
 import { STUDENT_REPOSITORY_KEY } from './domain/interfaces/student-repository.interface';
 import { StudentRepository } from './infrastructure/database/student.postgresql.repository';
 import { StudentMapper } from './application/mapper/student.mapper';
+import { CareerModule } from '../career/career.module';
 
 const studentServiceProvider = {
   provide: STUDENT_SERVICE_KEY,
@@ -20,7 +21,11 @@ const studentRepositoryProvider = {
 };
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forFeature([StudentEntity])],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([StudentEntity]),
+    CareerModule,
+  ],
   controllers: [StudentController],
   providers: [studentServiceProvider, studentRepositoryProvider, StudentMapper],
 })
