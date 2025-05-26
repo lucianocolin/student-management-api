74294e4eb16ccc4df3954d5a34312679d6bdd9bd
lucianocolin <lucianocolin99@gmail.com>
2025-05-25 21:57:25 -0300

    Add career domain in student

--- src/modules/student/application/dto/student-response.dto.ts
+++ src/modules/student/application/dto/student-response.dto.ts
@@ -1,10 +1,12 @@
+import { Career } from 'src/modules/career/domain/career.domain';
+
 export class StudentResponseDto {
   id?: string;
   userId?: string;
   fullName: string;
   email: string;
   collegeId: number;
-  career: string;
+  career: Career;
   subjects: string[];
   qualifications?: number[];
   createdAt?: Date;
