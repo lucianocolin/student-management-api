5312aebff12f21346e3758b85a254b1c22a26032
lucianocolin <lucianocolin99@gmail.com>
2025-05-26 21:16:10 -0300

    Add subject relation in career entity

--- src/modules/career/infrastructure/career.entity.ts
+++ src/modules/career/infrastructure/career.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { CAREER_NAME } from '../domain/career.name';
 import { StudentEntity } from '../../student/infrastructure/database/student.entity';
+import { SubjectEntity } from '../../subject/infrastructure/database/subject.entity';
 
 @Entity(CAREER_NAME)
 export class CareerEntity {
@@ -10,8 +11,12 @@ export class CareerEntity {
   @Column({ type: 'text', unique: true })
   name: string;
 
-  // Add relationship with Subject
-
   @OneToMany(() => StudentEntity, (student) => student.career)
   student: StudentEntity;
+
+  @OneToMany(() => SubjectEntity, (subject) => subject.career, {
+    cascade: true,
+    eager: true,
+  })
+  subjects: SubjectEntity[];
 }
