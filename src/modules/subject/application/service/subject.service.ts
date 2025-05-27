979a743e01b4478661c9e2171d417ebbaa0e413b
lucianocolin <lucianocolin99@gmail.com>
2025-05-26 21:14:03 -0300

    Add subject service and its interface

---
+++ src/modules/subject/application/service/subject.service.ts
@@ -0,0 +1,47 @@
+import {
+  ISubjectRepository,
+  SUBJECT_REPOSITORY_KEY,
+} from './../../domain/interfaces/subject-repository.interface';
+import { Inject, Injectable } from '@nestjs/common';
+import { ISubjectService } from '../../domain/interfaces/subject-service.interface';
+import { SubjectResponseDto } from '../dto/subject-response.dto';
+import { SubjectMapper } from '../mapper/subject.mapper';
+import { CreateSubjectDto } from '../dto/create-subject.dto';
+import {
+  CAREER_SERVICE_KEY,
+  ICareerService,
+} from '../../../career/domain/interfaces/career-service.interface';
+
+@Injectable()
+export class SubjectService implements ISubjectService {
+  constructor(
+    @Inject(SUBJECT_REPOSITORY_KEY)
+    private readonly subjectRepository: ISubjectRepository,
+    private readonly subjectMapper: SubjectMapper,
+    @Inject(CAREER_SERVICE_KEY)
+    private readonly careerService: ICareerService,
+  ) {}
+
+  async getAll(): Promise<SubjectResponseDto[]> {
+    const dbSubjects = await this.subjectRepository.findAll();
+
+    return dbSubjects.map((dbSubject) =>
+      this.subjectMapper.fromSubjectToSubjectResponseDto(dbSubject),
+    );
+  }
+
+  async create(
+    createSubjectDto: CreateSubjectDto,
+  ): Promise<SubjectResponseDto> {
+    const career = await this.careerService.getOneById(
+      createSubjectDto.careerId,
+    );
+
+    const dbSubject = await this.subjectRepository.create({
+      ...createSubjectDto,
+      career,
+    });
+
+    return this.subjectMapper.fromSubjectToSubjectResponseDto(dbSubject);
+  }
+}
