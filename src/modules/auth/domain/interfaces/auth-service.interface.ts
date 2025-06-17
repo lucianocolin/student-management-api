618b03778ac78fc1c0c70c791cf7264699ee2db3
lucianocolin <lucianocolin99@gmail.com>
2025-06-16 21:30:14 -0300

    Add approve registration method and modify login handling

--- src/modules/auth/domain/interfaces/auth-service.interface.ts
+++ src/modules/auth/domain/interfaces/auth-service.interface.ts
@@ -8,6 +8,7 @@ export const AUTH_SERVICE_KEY = 'AUTH_SERVICE';
 export interface IAuthService {
   register(registerUserDto: RegisterUserDto): Promise<UserResponseDto>;
   login(loginUserDto: LoginUserDto): Promise<UserResponseDto>;
+  approveRegistrationRequest(userId: string): Promise<UserResponseDto>;
   updateUser(
     userId: string,
     updateUserDto: UpdateUserDto,
